1c597f72fd5259230c801b6896c372cfdedacb73
amatsushita-prodam <amatsushita@prodam.sp.gov.br>
2025-12-13 12:22:13 -0300

    feat: add webhook list

--- web/src/components/sidebar.tsx
+++ web/src/components/sidebar.tsx
@@ -1,5 +1,6 @@
 import { CopyIcon } from 'lucide-react'
 import { IconButton } from './ui/icon-button'
+import { WebhooksList } from './webhooks-list'
 
 export function Sidebar() {
   return (
@@ -17,6 +18,8 @@ export function Sidebar() {
         </div>
         <IconButton icon={<CopyIcon className="size-4" />} />
       </div>
+
+      <WebhooksList />
     </div>
   )
 }
