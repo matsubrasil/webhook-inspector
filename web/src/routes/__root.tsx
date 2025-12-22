c27d2f05323a149c208ab96fc3df51a8b03af302
Alexadre Seiji Matsushita <matsuboy@outlook.com>
2025-12-22 17:52:26 -0300

    feat: add new page - empty webhook

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,11 +1,23 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels'
+import { Sidebar } from '../components/sidebar'
 
 const queryClient = new QueryClient()
 
 const RootLayout = () => (
   <QueryClientProvider client={queryClient}>
-    <Outlet />
+    <div className="h-screen bg-zinc-900">
+      <PanelGroup direction="horizontal">
+        <Panel defaultSize={20} minSize={15} maxSize={40}>
+          <Sidebar />
+        </Panel>
+        <PanelResizeHandle className="w-px bg-zinc-700 transition-colors duration-150 data-[resize-handle-state=hover]:bg-zin-600" />
+        <Panel defaultSize={80} minSize={60}>
+          <Outlet />
+        </Panel>
+      </PanelGroup>
+    </div>
   </QueryClientProvider>
 )
 
