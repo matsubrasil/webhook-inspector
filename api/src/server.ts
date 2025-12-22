956ab1c2a77419189556f985bf6db1c77206a982
Alexadre Seiji Matsushita <matsuboy@outlook.com>
2025-12-22 11:37:18 -0300

    feat: add capture-webhook

--- api/src/server.ts
+++ api/src/server.ts
@@ -9,6 +9,7 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { env } from '@/env'
+import { captureWebhook } from '@/routes/capture-webhook'
 import { deleteWebhook } from '@/routes/delete-webhook'
 import { getWebhook } from '@/routes/get-webhook'
 import { listWebhooks } from '@/routes/list-webhooks'
@@ -42,6 +43,7 @@ app.register(ScalarApiReference, {
 app.register(listWebhooks)
 app.register(getWebhook)
 app.register(deleteWebhook)
+app.register(captureWebhook)
 
 app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('🔥HTTP server running on http://localhost:3333')
